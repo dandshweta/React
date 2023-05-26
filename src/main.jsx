46f940558e579727fef2caaa767aa6688e86938b
shweta dand <dandshweta2017@gmail.com>
2023-05-26 15:28:49 +0530

    updated

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Homepage from './Homepage.jsx'
+import HomepageView from './HomepageView.jsx'
+// import TextDemo from './TextDemo.jsx'
+
+
+// import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Homepage />
+    <HomepageView/>
+    {/* <TextDemo/> */}
+  </React.StrictMode>,
+)
